194ada09955bd208ef2f17f94b248822571d0920
shota yonemura <kathewka@gmail.com>
2017-05-07 17:04:11 +0900

    WIP テンプレート準備

--- src/index.php
+++ src/index.php
@@ -1,6 +1,13 @@
 <?php
+require_once dirname(__FILE__) . '/classes/Entry.php';
 
-require_once './classes/Entry.php';
+// エントリを読み込み
+$entries = Entry::getEntries();
+// テンプレートに反映
+chdir(dirname(__FILE__) . '/template');
+require_once dirname(__FILE__) . '/template/template.php';
+
+
 echo '<pre>';
-print_r(Entry::getEntries());
+print_r($entries);
 echo '</pre>';
---
+++ src/template/template.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>EZ BLOG</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="copyright" content="Sample User"> -->
+    <!-- <meta name="description" content="ここにサイト説明を入れます"> -->
+    <!-- <meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５"> -->
+    <link rel="stylesheet" href="./template/css/style.css">
+    <!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+    <script type="text/javascript" src="./template/js/openclose.js"></script>
+  </head>
+  <body>
+    <div id="container">
+      <header>
+        <h1 id="logo">
+          <a href="index.html">
+            EZ BLOG
+          </a>
+        </h1>
+      </header>
+      <!-- <nav id="menubar">
+        <ul>
+          <li>
+            <a href="index">HOME</a>
+          </li>
+        </ul>
+      </nav> -->
+      <div id="contents">
+        <div id="main">
+          <section>
+            <h2>無料テンプレートのご利用前に必ずお読み下さい</h2>
+            <p>このテンプレートは、
+              <a href="http://template-party.com/">Template Party</a>にて無料配布している『個人サイト向けシンプルデザイン 無料ホームページテンプレート tp_simple10』です。必ずダウンロード先のサイトの利用規約をご一読の上でご利用下さい。
+            </p>
+            <p>
+              <span class="color1">■
+                <strong>HP最下部の著作表示『Web Design:Template-Party』は無断で削除しないで下さい。</strong>
+              </span>
+              <br>わざと見えなく加工する事も禁止です。
+            </p>
+            <p>
+              <span class="color1">■
+                <strong>どうしても下部の著作を外したい場合は</strong>
+              </span>
+              <br>
+              <a href="http://template-party.com/">Template-Party</a>の
+              <a href="http://template-party.com/member.html">ライセンス契約</a>を行う事でHP下部の著作を外す事ができます。おまけ特典として、制作時Photoshopファイルももらえます。
+            </p>
+            <section>
+              <h3>当テンプレートはhtml5+CSS3(レスポンシブWEBデザイン)です</h3>
+              <p>パソコン、タブレット、スマートフォンに対応しています。各デバイスごとの設定変更はcssフォルダ内にある各cssファイルで行って下さい。
+                <br>尚、新しいブラウザ(IEならIE10以降)でないと全ての効果を確認する事はできません。マークアップなどはご自身の判断で変更してご利用下さい。
+              </p>
+            </section>
+            <section>
+              <h3>当テンプレートの詳しい使い方</h3>
+              <p>
+                <a href="about.html">当テンプレートの詳しい使い方はこちらをご覧下さい。</a>
+              </p>
+            </section>
+          </section>
+          <section id="new">
+            <h2 id="newinfo_hdr" class="close">更新情報・お知らせ</h2>
+            <dl id="newinfo">
+              <dt>20XX/00/00</dt>
+              <dd>ホームページリニューアル
+                <span class="newicon">NEW</span>
+              </dd>
+              <dt>20XX/00/00</dt>
+              <dd>ホームページリニューアル</dd>
+              <dt>20XX/00/00</dt>
+              <dd>ホームページリニューアル</dd>
+              <dt>20XX/00/00</dt>
+              <dd>ホームページリニューアル</dd>
+              <dt>20XX/00/00</dt>
+              <dd>ホームページリニューアル</dd>
+              <dt>20XX/00/00</dt>
+              <dd>ホームページリニューアル</dd>
+              <dt>20XX/00/00</dt>
+              <dd>ホームページリニューアル</dd>
+            </dl>
+          </section>
+        </div>
+        <!--/main-->
+        <div id="sub">
+          <nav class="box1">
+            <h2>SUB MENU</h2>
+            <ul class="submenu mb10">
+              <li>
+                <a href="#">主要リンクサンプル</a>
+              </li>
+              <li>
+                <a href="#">主要リンクサンプル</a>
+              </li>
+              <li>
+                <a href="#">主要リンクサンプル</a>
+              </li>
+              <li>
+                <a href="#">主要リンクサンプル</a>
+              </li>
+            </ul>
+          </nav>
+          <aside>
+            <h2>関連情報</h2>
+            <ul class="submenu">
+              <li>
+                <a href="#">関連情報リンク</a>
+              </li>
+              <li>
+                <a href="#">関連情報リンク</a>
+              </li>
+              <li>
+                <a href="#">関連情報リンク</a>
+              </li>
+              <li>
+                <a href="#">関連情報リンク</a>
+              </li>
+            </ul>
+          </aside>
+          <div class="box1">
+            <section>
+              <h2>この見出しはh2タグです</h2>
+              <p>このボックスは、class="box1"と指定すれば出ます。ここに画像を置く場合、PC環境だと幅198pxまで。それ以上だと自動縮小されます。</p>
+            </section>
+          </div>
+          <!--/box1-->
+          <section>
+            <h2>この見出しはh2タグです</h2>
+            <p>ここのスペースも使えます。ここに画像を置く場合、PC環境だと幅230pxまで。それ以上だと自動縮小されます。</p>
+          </section>
+        </div>
+        <!--/sub-->
+        <p id="pagetop">
+          <a href="#">↑</a>
+        </p>
+      </div>
+      <!--/contents-->
+    </div>
+    <!--/container-->
+    <footer>
+      <small>Copyright&copy;
+        <a href="index.html">SAMPLE GALLERY</a>All Rights Reserved.
+      </small>
+      <span class="pr">
+        <a href="http://template-party.com/" target="_blank">Web Design:Template-Party</a>
+      </span>
+    </footer>
+    <!--スライドショースクリプト-->
+    <script type="text/javascript" src="js/slide_simple_pack.js"></script>
+    <!--スマホ用更新情報-->
+    <script type="text/javascript">
+if (OCwindowWidth() < 480) {
+	open_close("newinfo_hdr", "newinfo");
+}
+</script>
+  </body>
+</html>
